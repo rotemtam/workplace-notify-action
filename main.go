86208c01fd64eb8095d176d9f75b0a5c222d90a5
Rotem Tamir <rotemtamir@gmail.com>
2023-11-24 11:36:26 +0200

    init

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"github.com/sethvargo/go-githubactions"
 )
 
-const cloudDomainPublic = "https://gh-api.atlasgo.cloud"
-
 func main() {
 	act := githubactions.New()
 	act.Infof("Hello, world!")
